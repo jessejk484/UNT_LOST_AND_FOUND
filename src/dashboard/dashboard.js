133760f582a53317212f6d60713e657e55d0809d
abhi147p <abhishekpuppala60@gmail.com>
2023-11-27 23:22:01 -0600

    Shipment signout changes

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,27 +1,29 @@
-function redirectToGetForm() {
-    window.location.href = "/Users/puppa/All_Git_Files/unt_lost_and_found/src/getPage/getPage.html";
-}
-function redirectToLostForm() {
-    window.location.href = "/Users/puppa/All_Git_Files/unt_lost_and_found/src/lostForm/lostForm.html";
-}
-const loginBtn = document.querySelector('.js-login-button');
-
-const loginModal = document.querySelector('.js-login-modal');
-
-loginBtn.addEventListener('click', () => {
-    console.log("Entered")
-    const isOpen = loginModal.classList.contains('is-open');
-
-    if (isOpen) {
-
-        loginModal.classList.remove('is-open');
-
+document.addEventListener('DOMContentLoaded', function () {
+    function redirectToGetForm() {
+        window.location.href = "/Users/puppa/All_Git_Files/unt_lost_and_found/src/getPage/getPage.html";
     }
-
-    else {
-
-        loginModal.classList.add('is-open');
-
+    function redirectToLostForm() {
+        window.location.href = "/Users/puppa/All_Git_Files/unt_lost_and_found/src/lostForm/lostForm.html";
     }
+    const loginBtn = document.querySelector('.js-login-button');
 
+    const loginModal = document.querySelector('.js-login-modal');
+
+    loginBtn.addEventListener('click', () => {
+        console.log("Entered")
+        const isOpen = loginModal.classList.contains('is-open');
+
+        if (isOpen) {
+
+            loginModal.classList.remove('is-open');
+
+        }
+
+        else {
+
+            loginModal.classList.add('is-open');
+
+        }
+
+    });
 });
